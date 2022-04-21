13a7f399348bc9113cb3eb90c0ecfa8fdea694c8
BuildTools <simenfhe@stud.ntnu.no>
2022-04-21 20:28:50 +0200

    prepare for fog

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-let options = {
-    root: document.querySelectior('#scrollArea'), //mulig null
-    rootMargin: '0px',
-    threshold: 1.0
+const section = document.querySelector('#s_mario');
+
+const options = {
+    threshold:.5
+};
+
+const observer = new IntersectionObserver(function(entries, observer){
+    entries.forEach(entry => {
+        let sCheck = console.log(entry.isIntersecting)
+        if(entry.isIntersecting){
+            console.log("if")
+            section.style.animationDelay ="2s"
+            section.style.animationIterationCount = "1"
+        }
+        else{
+            console.log("else")
+        }
+})
+} ,options);
+
+observer.observe(section)
+
+function resetAnimationSimen(){
+    window.location.reload()
 }
-
-function simenAnimation(){
-
-}
-
-let observer = new IntersectionObserver(simenAnimation, options)
-
-let target = document.querySelector('mario');
-observer.observe(target);
 
